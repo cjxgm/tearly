8e301075def4e2b7e31ca886fba7e325a39ac791
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2017-01-20 17:00:11 +0800

    webdav: + mkcol

--- webdav.js
+++ webdav.js
@@ -18,6 +18,12 @@ module-type: library
         return new Promise(resolve => setTimeout(resolve, 1000));
     };
 
+    WebDAV.prototype.mkcol = function(path) {
+        if (!path.endsWith("/")) throw new Error("Path for MKCOL must end with a slash: " + path);
+        console.log("DAV-MKCOL", path);
+        return new Promise(resolve => setTimeout(resolve, 1000));
+    };
+
     this.WebDAV = WebDAV;
 }).call(exports);
 
