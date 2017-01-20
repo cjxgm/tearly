d0522e6b5870efc9caec1223cafa4d505ef3c544
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2017-01-20 22:14:51 +0800

    implement dav

--- webdav.js
+++ webdav.js
@@ -10,19 +10,75 @@ module-type: library
 
     WebDAV.prototype.put = function(path, text) {
         console.log("DAV-PUT", path, { text: text });
-        return new Promise(resolve => setTimeout(resolve, 1000));
+
+        return new Promise((resolve, reject) => {
+            var req = this.xhr(reject,
+                    req => resolve(),
+                    req => {
+                        reject(new Error(req.statusText + "\n" + req.responseText));
+                    });
+            req.open("PUT", path);
+            req.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+            req.send(text);
+        });
     };
 
     WebDAV.prototype.delete = function(path) {
         console.log("DAV-DELETE", path);
-        return new Promise(resolve => setTimeout(resolve, 1000));
+
+        return new Promise((resolve, reject) => {
+            var req = this.xhr(reject,
+                    req => resolve(true),
+                    req => {
+                        if (req.status === 404) {     // file not found
+                            resolve(false);
+                        } else {
+                            reject(new Error(req.statusText + "\n" + req.responseText));
+                        }
+                    });
+            req.open("DELETE", path);
+            req.send();
+        });
     };
 
     WebDAV.prototype.mkcol = function(path) {
-        if (!path.endsWith("/")) throw new Error("Path for MKCOL must end with a slash: " + path);
         console.log("DAV-MKCOL", path);
-        return new Promise(resolve => setTimeout(resolve, 1000));
+        if (!path.endsWith("/")) throw new Error("Path for MKCOL must end with a slash: " + path);
+
+        return new Promise((resolve, reject) => {
+            var req = this.xhr(reject,
+                    req => resolve(true),
+                    req => {
+                        if (req.status === 405) {     // maybe collection exists already
+                            resolve(false);
+                        } else {
+                            reject(new Error(req.statusText + "\n" + req.responseText));
+                        }
+                    });
+            req.open("MKCOL", path);
+            req.send();
+        });
     };
+
+    WebDAV.prototype.xhr = function(reject, ok, fail) {
+        var req = new XMLHttpRequest();
+        req.onload = function() {
+            if (this.status >= 200 && this.status < 300) {
+                ok(this);
+            } else {
+                fail(this);
+            }
+        };
+        req.onerror = function (ev) {
+            console.error(ev);
+            reject(new Error("interrupted"));
+        };
+        req.onabort = function (ev) {
+            console.error(ev);
+            reject(new Error("abort"));
+        };
+        return req;
+    }
 
     this.WebDAV = WebDAV;
 }).call(exports);
